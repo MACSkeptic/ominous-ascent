1d4971ac38988792394ea6974868513b94d020db
mozair <macskeptic@gmail.com>
2013-01-30 22:42:42 -0200

    small refactoring on engine test

--- tests/engine/engine_test.js
+++ tests/engine/engine_test.js
@@ -2,13 +2,10 @@ define(function (require) {
   var engine = require('../../lib/engine'),
       support = require('../../lib/support');
 
-
-  module('engine - first invocation');
+  module('engine', { setup: support.globalStubs.$ });
 
   test('var args callbacks only on dom ready', function () {
     var draw = sinon.spy(), update = sinon.spy(), game = sinon.spy();
-
-    support.globalStubs.$();
 
     engine.start({ draw: draw }, { update: update }, { game: game });
 

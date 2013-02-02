75acf16908dd480695dcf352fa24ba374ac912f1
mozair <macskeptic@gmail.com>
2013-02-02 13:14:12 -0200

    added loading bar to splash screen

--- tests/scene/splash_screen_test.js
+++ tests/scene/splash_screen_test.js
@@ -1,10 +1,87 @@
 define(function (require) {
   var splashScreen = require('../../lib/scene/splash_screen');
+  var imageLoader = require('../../lib/asset').imageLoader;
 
-  module('splash screen');
+  module('splash screen', {
+    setup: function () {
+      splashScreen.initialised = false;
+      sinon.spy(imageLoader, 'init');
+      sinon.stub(imageLoader, 'start');
+    },
+    teardown: function () {
+      imageLoader.init.restore();
+      imageLoader.start.restore();
+    }
+  });
 
   test('title', function () {
-    equal(splashScreen.layers.foreground.get('Text').length, 1);
-    equal(splashScreen.layers.foreground.get('Text')[0].getText(), 'ominous ascent');
+    var title = splashScreen.layers.foreground.get('Text')[0];
+    equal(title.getY(), 100);
+    equal(title.getX(), splashScreen.width / 2 - title.getWidth() / 2);
+    equal(title.getText(), 'ominous ascent');
+  });
+
+  test('dimensions', function () {
+    equal(splashScreen.width, 1000);
+    equal(splashScreen.height, 500);
+  });
+
+  test('init/start image loader only once', function () {
+    splashScreen.update({});
+    equal(imageLoader.init.callCount, 1);
+    equal(imageLoader.start.callCount, 1);
+    splashScreen.update({});
+    equal(imageLoader.init.callCount, 1);
+    equal(imageLoader.start.callCount, 1);
+  });
+
+  test('load sprites', function () {
+    splashScreen.update({});
+    deepEqual(
+      imageLoader.init.lastCall.args[0].imagesToLoad,
+      ['/media/sprites/target.gif', '/media/sprites/demon.png']
+    );
+  });
+
+  test('store sprites on the state', function () {
+    var state = {};
+    var sprites = ['/media/sprites/target.gif', '/media/sprites/demon.png'];
+
+    splashScreen.update(state);
+    imageLoader.init.lastCall.args[0].complete(sprites);
+
+    deepEqual(state.sprites, sprites);
+  });
+
+  test('loading bar background', function () {
+    equal(splashScreen.layers.foreground.get('#loading-bar-background').length, 1);
+
+    var bar = splashScreen.layers.foreground.get('#loading-bar-background')[0];
+    equal(bar.getWidth(), 900);
+    equal(bar.getX(), 50);
+    equal(bar.getHeight(), 100);
+    equal(bar.getY(), 350);
+  });
+
+  test('loading bar foreground', function () {
+    splashScreen.update({});
+
+    equal(splashScreen.layers.foreground.get('#loading-bar-foreground').length, 1);
+
+    var bar = splashScreen.layers.foreground.get('#loading-bar-foreground')[0];
+
+    equal(bar.getWidth(), 0);
+    equal(bar.getX(), 50);
+    equal(bar.getHeight(), 100);
+    equal(bar.getY(), 350);
+
+    imageLoader.init.lastCall.args[0].progress(0.1);
+    equal(bar.getWidth(), 900 * 0.1);
+
+    imageLoader.init.lastCall.args[0].progress(0.9);
+    equal(bar.getWidth(), 900 * 0.9);
+
+    imageLoader.init.lastCall.args[0].progress(1);
+    equal(bar.getWidth(), 900 * 1);
   });
 });

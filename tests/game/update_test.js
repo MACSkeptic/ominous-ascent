e57dd25bd0d2889932fd34cc68f88e1038845fb0
mozair <macskeptic@gmail.com>
2013-02-02 19:12:10 -0200

    calls handleInput on the scene if it has it

--- tests/game/update_test.js
+++ tests/game/update_test.js
@@ -10,7 +10,14 @@ define(function (require) {
     ok(callback.calledWith(state));
   });
 
-  test('does not break if the current scene not have an update callback', function () {
+  test('handle input current scene', function () {
+    var callback = sinon.spy();
+    var state = { currentScene: { handleInput: callback } };
+    update(state);
+    ok(callback.calledWith(state));
+  });
+
+  test('does not break if the current scene not have an update/handleInput callback', function () {
     expect(0);
     update({ currentScene: { } });
   });

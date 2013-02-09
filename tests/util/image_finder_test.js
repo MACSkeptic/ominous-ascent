730556ab3f096c71679835e9ec9163493176ebda
mozair <macskeptic@gmail.com>
2013-02-09 21:49:14 -0200

    added functionality to draw images

---
+++ tests/util/image_finder_test.js
@@ -0,0 +1,15 @@
+define(function (require) {
+  var imageFinder = require('../../lib/util/image_finder');
+
+  module('image finder');
+
+  test('finds image', function () {
+    var images = [
+      { src: '/wrong/image.png' },
+      { src: '/rigth/image.png' },
+      { src: '/right/image.gif' }
+    ];
+
+    equal(imageFinder(images, /right\/image\.png/), images[1]);
+  });
+});

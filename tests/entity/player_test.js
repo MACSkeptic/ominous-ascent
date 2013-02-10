c654d5ebf38a1061ec4bc74cdc44bb284e38b921
mozair <macskeptic@gmail.com>
2013-02-10 00:07:29 -0200

    player understands input and doesn't need the sprite nonsense to be created

--- tests/entity/player_test.js
+++ tests/entity/player_test.js
@@ -1,21 +1,46 @@
 define(function (require) {
   var player = require('../../lib/entity').player;
+  var instance;
 
-  var sprites = [new Image()];
-  sprites[0].onload = runTests;
-  sprites[0].src = '/media/sprites/demon.png';
-
-  function runTests() {
-    var instance;
-
-    function setup() {
-      instance = player(sprites, { x: 0, y: 0 });
-    }
-
-    module('player', { setup: setup });
-
-    test('speed', function () {
-      expect(0);
-    });
+  function setup() {
+    instance = player({ x: 0, y: 0 });
   }
+
+  module('player', { setup: setup });
+
+  test('move left', function () {
+    var pressed = sinon.stub();
+    pressed.returns(false);
+    pressed.withArgs('a').returns(true);
+
+    instance.handleInput({}, { keyPressed: pressed });
+    equal(instance.velocityX, -instance.velocity);
+  });
+
+  test('move right', function () {
+    var pressed = sinon.stub();
+    pressed.returns(false);
+    pressed.withArgs('d').returns(true);
+
+    instance.handleInput({}, { keyPressed: pressed });
+    equal(instance.velocityX, instance.velocity);
+  });
+
+  test('move up', function () {
+    var pressed = sinon.stub();
+    pressed.returns(false);
+    pressed.withArgs('w').returns(true);
+
+    instance.handleInput({}, { keyPressed: pressed });
+    equal(instance.velocityY, -instance.velocity);
+  });
+
+  test('move down', function () {
+    var pressed = sinon.stub();
+    pressed.returns(false);
+    pressed.withArgs('s').returns(true);
+
+    instance.handleInput({}, { keyPressed: pressed });
+    equal(instance.velocityY, instance.velocity);
+  });
 });
--- tests/scene/first_screen_test.js
+++ tests/scene/first_screen_test.js
@@ -23,52 +23,17 @@ define(function (require) {
       equal(get(instance, 'player').length, 1);
     });
 
-    test('player move left', function () {
+    test('handle input', function () {
       var instance = firstScreen(state);
       var player = get(instance, 'player')[0];
-      var x = player.x;
-      var pressed = sinon.stub();
-      pressed.returns(false);
-      pressed.withArgs('a').returns(true);
+      var input = { keyPressed: function () {}, clicked: function () {} };
+      sinon.spy(player, 'handleInput');
 
-      instance.handleInput({}, { keyPressed: pressed });
-      equal(player.x, x - playerSpeed);
-    });
+      instance.handleInput(state, input);
 
-    test('player move right', function () {
-      var instance = firstScreen(state);
-      var player = get(instance, 'player')[0];
-      var x = player.x;
-      var pressed = sinon.stub();
-      pressed.returns(false);
-      pressed.withArgs('d').returns(true);
-
-      instance.handleInput({}, { keyPressed: pressed });
-      equal(player.x, x + playerSpeed);
-    });
-
-    test('player move up', function () {
-      var instance = firstScreen(state);
-      var player = get(instance, 'player')[0];
-      var y = player.y;
-      var pressed = sinon.stub();
-      pressed.returns(false);
-      pressed.withArgs('w').returns(true);
-
-      instance.handleInput({}, { keyPressed: pressed });
-      equal(player.y, y - playerSpeed);
-    });
-
-    test('player move down', function () {
-      var instance = firstScreen(state);
-      var player = get(instance, 'player')[0];
-      var y = player.y;
-      var pressed = sinon.stub();
-      pressed.returns(false);
-      pressed.withArgs('s').returns(true);
-
-      instance.handleInput({}, { keyPressed: pressed });
-      equal(player.y, y + playerSpeed);
+      equal(player.handleInput.callCount, 1);
+      equal(player.handleInput.lastCall.args[0], state);
+      equal(player.handleInput.lastCall.args[1], input);
     });
   }
 });

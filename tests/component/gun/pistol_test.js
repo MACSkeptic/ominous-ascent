a100a938e793f4e44617a1ce61ad0c0f5d090ef0
mozair <macskeptic@gmail.com>
2013-02-09 22:26:58 -0200

    pistol accumulates time since last shot

--- tests/component/gun/pistol_test.js
+++ tests/component/gun/pistol_test.js
@@ -1,16 +1,36 @@
 define(function (require) {
   var pistol = require('../../../lib/component').gun.pistol;
-  var shot;
+  var bullet;
+  var state;
+  var scene;
+  var entities;
   var instance;
 
   function setup() {
-    shot = sinon.stub();
-    instance = pistol({}, shot);
+    entities = [];
+    scene = { entities: entities };
+    state = { currentScene: scene };
+    bullet = sinon.stub();
+    instance = pistol({}, bullet);
   }
 
   module('pistol', { setup: setup });
 
-  test('updates', function () {
-    ok(instance.update);
+  test('updates time since last shot', function () {
+    equal(instance.timeSinceLastShot, 0);
+    instance.shoot(state);
+    equal(instance.timeSinceLastShot, 0);
+    state.elapsed = 10;
+    instance.update(state);
+    equal(instance.timeSinceLastShot, 10);
+  });
+
+  test('shoots', function () {
+    var bulletInstance = 'instance of a bullet';
+    bullet.returns(bulletInstance);
+
+    instance.shoot(state);
+
+    equal(entities[0], bulletInstance);
   });
 });

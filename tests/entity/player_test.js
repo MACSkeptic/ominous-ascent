e89f29591db6b38942c68ca96d08305c9fdc365a
mozair <macskeptic@gmail.com>
2013-02-16 23:48:23 -0200

    player now handles clicks by himself, shooting at the cursor position

--- tests/entity/player_test.js
+++ tests/entity/player_test.js
@@ -13,7 +13,7 @@ define(function (require) {
     pressed.returns(false);
     pressed.withArgs('a').returns(true);
 
-    instance.handleInput({}, { keyPressed: pressed });
+    instance.handleInput({}, { keyPressed: pressed, clicked: sinon.stub() });
     equal(instance.velocityX, -instance.velocity);
   });
 
@@ -22,7 +22,7 @@ define(function (require) {
     pressed.returns(false);
     pressed.withArgs('d').returns(true);
 
-    instance.handleInput({}, { keyPressed: pressed });
+    instance.handleInput({}, { keyPressed: pressed, clicked: sinon.stub() });
     equal(instance.velocityX, instance.velocity);
   });
 
@@ -31,7 +31,7 @@ define(function (require) {
     pressed.returns(false);
     pressed.withArgs('w').returns(true);
 
-    instance.handleInput({}, { keyPressed: pressed });
+    instance.handleInput({}, { keyPressed: pressed, clicked: sinon.stub() });
     equal(instance.velocityY, -instance.velocity);
   });
 
@@ -40,7 +40,24 @@ define(function (require) {
     pressed.returns(false);
     pressed.withArgs('s').returns(true);
 
-    instance.handleInput({}, { keyPressed: pressed });
+    instance.handleInput({}, { keyPressed: pressed, clicked: sinon.stub() });
     equal(instance.velocityY, instance.velocity);
+  });
+
+  test('shoots at click', function () {
+    var click = { x: 10, y: 20 };
+    var scene = 'scene';
+    var state = { currentScene: scene };
+    var clicked = sinon.stub();
+    clicked.returns(false);
+    clicked.withArgs(scene).returns(click);
+
+    sinon.stub(instance.components[1], 'shoot');
+
+    instance.handleInput(state, { keyPressed: sinon.stub(), clicked: clicked });
+
+    equal(instance.components[1].shoot.callCount, 1);
+    equal(instance.components[1].shoot.lastCall.args[0], state);
+    equal(instance.components[1].shoot.lastCall.args[1], click);
   });
 });
